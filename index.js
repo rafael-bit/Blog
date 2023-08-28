d8dbaa43c1bbb996b3b413ccb023d88a14d74e40
rafael-bit <raquila743@gmail.com>
2023-08-28 14:35:24 -0300

    Init Sequelize

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+const Sequelize = require("sequelize");
+const sequelize  = new Sequelize('blog', 'root', '123456', {
+    host: "localhost",
+    dialect: "mysql"
+});
+
 const express = require("express");
 const app = express();
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + "/index.html")
-})
+sequelize.authenticate().then(function(){
+    console.log("Conectado com Sucesso !")
+}).catch(function(erro){
+    console.log("Falha ao se conectar: " + erro);
+});
 
-app.listen(4321, function(){console.log("Rodando...")})
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(4321, function(){console.log("Rodando...")});
