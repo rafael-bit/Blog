45a0fc791451bdad4e51333cfaea91d6d02bf8c4
rafael-bit <raquila743@gmail.com>
2023-09-13 22:05:02 -0300

    handlebars

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
-const app = express()
-const handlebars = require('express-handlebars')
+const app = express();
+const handlebars = require('express-handlebars');
+const Sequelize = require("sequelize");
+const sequelize  = new Sequelize('blog', 'root', '123456', {
+    host: "localhost",
+    dialect: "mysql"
+});
 
-app.engine('handlebars')
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set("view engine", "handlebars")
+
+app.listen(4321, function(){
+    console.log("Servidor Rodando")
+})
